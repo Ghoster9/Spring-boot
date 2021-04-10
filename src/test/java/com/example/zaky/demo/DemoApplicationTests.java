45c193b585d7f6514108cfb4cf2e2d562b4909ea
ahhzaky <siahmadzaky@gmail.com>
2021-04-11 00:22:47 +0700

    :tada: initial project

---
+++ src/test/java/com/example/zaky/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zaky.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
