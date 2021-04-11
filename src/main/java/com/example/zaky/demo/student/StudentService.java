dcda516ea76108afa35ed40ac38bbd334704b023
ahhzaky <siahmadzaky@gmail.com>
2021-04-11 07:05:39 +0700

    BISNIS LAYER

---
+++ src/main/java/com/example/zaky/demo/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.example.zaky.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Jack",
+                        "jack.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
+    }
+}
